06f049b3c391470dc040d3c9994921e1eff98d68
Bartek Lichaj <Bartek Lichaj>
2016-12-09 21:22:12 +0100

    dodanie wciec i srednika 2

--- zadanie 9.2/js/scripts.js
+++ zadanie 9.2/js/scripts.js
@@ -6,9 +6,9 @@ span.each(function(index, element) {
 }); 
 
 var paragraphs = $('p');
-	paragraphs.each(function(index, element) {
+paragraphs.each(function(index, element) {
 
-var button = '<button class="btn" data-tmp="' + index +'">Click me</button>'
+	var button = '<button class="btn" data-tmp="' + index +'">Click me</button>';
 	$(element).append(button);
 
 });
